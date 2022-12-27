a78f9034a378e02b525cb686ff0ae64b22d53f8a
dhruv <dhruvrayat50@gmail.com>
2022-12-28 10:49:49 +1300

    Feature: Added a token login for ADMIN users, and added a `can_access_dashboard` attribute to all tokens, and only admins get them

--- src/core/authentication/login.ts
+++ src/core/authentication/login.ts
@@ -14,38 +14,95 @@ type UserToken = {
   username: string;
   role: string;
   password: string;
+  can_access_dashboard: boolean;
 };
 
 type PostUser = {
   username: string;
   role: string;
+  can_access_dashboard: boolean;
 };
 
 type Token = {
   token: string;
 };
 
-router.post("/admin", async (req: Request, res: Response) => {});
+router.post("/admin", async (req: Request, res: Response) => {
+  const query_get_password =
+    "select password, role from admins where username=$1";
+  const values_get_password = [req.body.username];
 
-router.post("/user", async (req: Request, res: Response) => {
-  let query_get_password = "select password, role from users where username=$1";
-  let values_get_password = [req.body.username];
-
-  let items = [req.body.username, req.body.password];
+  const items = [req.body.username, req.body.password];
 
   for (let i = 0; i < items.length; i++)
     if (items[i] === undefined || items[i] === "")
       return res.send({ detail: "Provide all values" });
 
-  let sql_res_get_pass = await pool.query(
-    query_get_password,
-    values_get_password
-  );
+  let sql_res_get_pass;
+
+  try {
+    sql_res_get_pass = await pool.query(
+      query_get_password,
+      values_get_password
+    );
+  } catch (err: any) {
+    return res.status(500).send({ detail: err.stack });
+  }
+
+  if (sql_res_get_pass.rowCount === 0)
+    return res.status(400).send({ detail: "This admin does not exist" });
+
+  if (
+    !(await bcrypt_compare(
+      req.body.password,
+      sql_res_get_pass.rows[0].password
+    ))
+  )
+    return res.status(400).send({ detail: "Incorrect password" });
+
+  let user: UserToken = {
+    username: req.body.username,
+    role: sql_res_get_pass.rows[0].role,
+    password: sql_res_get_pass.rows[0].password,
+    can_access_dashboard: true,
+  };
+
+  let access_token: Token = {
+    token: create_token(user),
+  };
+
+  console.log("created admin with token: " + user.role);
+
+  return res.send({ adminAccessToken: access_token.token });
+});
+
+router.post("/user", async (req: Request, res: Response) => {
+  const query_get_password =
+    "select password, role from users where username=$1";
+  const values_get_password = [req.body.username];
+
+  const items = [req.body.username, req.body.password];
+
+  for (let i = 0; i < items.length; i++)
+    if (items[i] === undefined || items[i] === "")
+      return res.send({ detail: "Provide all values" });
+
+  let sql_res_get_pass;
+
+  try {
+    sql_res_get_pass = await pool.query(
+      query_get_password,
+      values_get_password
+    );
+  } catch (err: any) {
+    return res.status(500).send({ detail: err.stack });
+  }
 
   if (sql_res_get_pass.rowCount === 0) {
     let user: PostUser = {
       username: req.body.username,
       role: roles.CREATE_USER,
+      can_access_dashboard: false,
     };
 
     let access_token: Token = {
@@ -69,6 +126,7 @@ router.post("/user", async (req: Request, res: Response) => {
     username: req.body.username,
     role: sql_res_get_pass.rows[0].role,
     password: sql_res_get_pass.rows[0].password,
+    can_access_dashboard: false,
   };
 
   let access_token: Token = {
